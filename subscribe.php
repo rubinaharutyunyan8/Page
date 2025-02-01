5a32869b727eb87003e6c721e7d94196e13d9c59
rubinaharutyunyan8 <harutyunyanr249@gmail.com>
2025-02-01 21:28:37 +0400

    disabled zappier

--- subscribe.php
+++ subscribe.php
@@ -2,8 +2,8 @@
 $file = "/home/p9sbav7hz4ag/subscribers.txt";
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    // echo json_encode(["status" => "success", "message" => "Subscription successful!"]);
-    // return
+    echo json_encode(["status" => "success", "message" => "Subscription successful!"]);
+    return
     $email = trim($_POST['email']);
 
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
@@ -23,24 +23,24 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     //     }
     // }
 
-    if (file_put_contents($file, $entry, FILE_APPEND | LOCK_EX)) {
+    // if (file_put_contents($file, $entry, FILE_APPEND | LOCK_EX)) {
 
-        // Send to Zapier Webhook
-        $zapierWebhook = "https://hooks.zapier.com/hooks/catch/21508435/2fkewht";
-        $zapierData = json_encode(["email" => $email]);
+    //     // Send to Zapier Webhook
+    //     $zapierWebhook = "https://hooks.zapier.com/hooks/catch/21508435/2fkewht";
+    //     $zapierData = json_encode(["email" => $email]);
 
-        $ch = curl_init($zapierWebhook);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type: application/json"]);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $zapierData);
-        curl_exec($ch);
-        curl_close($ch);
+    //     $ch = curl_init($zapierWebhook);
+    //     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    //     curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type: application/json"]);
+    //     curl_setopt($ch, CURLOPT_POST, 1);
+    //     curl_setopt($ch, CURLOPT_POSTFIELDS, $zapierData);
+    //     curl_exec($ch);
+    //     curl_close($ch);
 
         echo json_encode(["status" => "success", "message" => "Subscription successful!"]);
-    } else {
-        echo json_encode(["status" => "error", "message" => "Failed to save email. Try again later."]);
-    }
+    // } else {
+        // echo json_encode(["status" => "error", "message" => "Failed to save email. Try again later."]);
+    // }
 } else {
     // Block direct access
     die(json_encode(["status" => "error", "message" => "Invalid request."]));
